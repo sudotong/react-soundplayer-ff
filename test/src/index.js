f2924df57d53debd742188cfea558caa19876792
Theoluwafemi <oluwafemidamilola21@gmail.com>
2020-12-24 14:21:50 +0100

    formatted documents

--- test/src/index.js
+++ test/src/index.js
@@ -1,8 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-import AudioPlayer from './AudioPlayer';
+import AudioPlayer from "./AudioPlayer";
+import WaveForm from "./Waveform";
 
-ReactDOM.render(<div className="audio-section">
-    <AudioPlayer mp3url="https://parse-server-ff.s3.amazonaws.com/ae5992f0f5bb1f259bafa41b3771e3bb_call12565815456dwwwwww795896232www-01b59bd3.mp3" />
-</div>, document.getElementById('root'));
+ReactDOM.render(
+  <div>
+    <div className="audio-section">
+      <AudioPlayer mp3url="https://parse-server-ff.s3.amazonaws.com/ae5992f0f5bb1f259bafa41b3771e3bb_call12565815456dwwwwww795896232www-01b59bd3.mp3" />
+    </div>
+
+    <div className="audio-section">
+      <WaveForm />
+    </div>
+  </div>,
+  document.getElementById("root")
+);
